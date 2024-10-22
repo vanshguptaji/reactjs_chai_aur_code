2d9558282ac601ced730fed10760457e1b37bfd2
vanshguptaji <vansh23164033@akgec.ac.in>
2024-10-22 23:58:00 +0530

    random problem occured in creating a function inside main.jsx

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,8 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const anotherElement = (
+  <a href="http://google.com" target='_blank'>
+  Visit google
+  </a>
+)
+
+reactDOM.createRoot(document.getElementById('root'))
+.render(
+    <anotherElement />
 )
